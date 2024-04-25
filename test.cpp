eb6ac930b91fb738f935e2c3678df8798851e3b3
icad08 <irsyadrm08@gmail.com>
2024-04-25 15:06:20 +0700

    menambahkan case 1

--- test.cpp
+++ test.cpp
@@ -133,7 +133,14 @@ int main(){
             cout << "5. Keluar" << endl;
             cout << "Pilihan: ";
             cin >> pilihan;
-            switch (pilihan)
+            switch (pilihan){
+                case 1:
+                addNode ();
+                cout << "data berhasil ditambahkan" << endl;
+                system ("pause");
+                system  ("cls");
+                break;
+            }
         }
     }
 
