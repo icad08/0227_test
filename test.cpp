c6ba2ff69047f7e4c8ee605f95a1b6904300b427
icad08 <irsyadrm08@gmail.com>
2024-04-25 14:10:44 +0700

    bikin member

--- test.cpp
+++ test.cpp
@@ -10,6 +10,13 @@ struct node {
 node* START =  NULL;
 
 void addNode (){
+    int nim;
+    string nama;
+    node* nodeBaru = new Node();
+    cout << "masukkan nim:";
+    cin >> nim;
+    cout <<"masukkan nama : ";
+    cin >> nama;
     
 }
 
